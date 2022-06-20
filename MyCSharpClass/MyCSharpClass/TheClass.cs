8d3195bf7c53675f245f6eadc91d8264e0e59622
orionsamuel <s.oliveira.15@hotmail.com>
2022-06-20 15:46:17 -0300

    Add files via upload

---
+++ MyCSharpClass/MyCSharpClass/TheClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace MyCSharpClass
+{
+    [ComVisible(true)]
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    [Guid("485B98AF-53D4-4148-B2BD-CC3920BF0ADF")]
+    public class TheClass
+    {
+        public String GetTheThing(String arg)
+        {
+            return arg + " the thing";
+        }
+    }
+}
